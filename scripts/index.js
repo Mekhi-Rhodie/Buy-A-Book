e1bbe9ae2dd097b205b4df451052fdb5f6998a18
Mekhi Rhodie <mekhirhodie@gmail.com>
2020-06-29 20:07:04 -0400

    Update index.js
    - Add jQuery toggle function to nav button.

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,19 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyD9K82L8GFGOkRcqQEJGNSj2PXQAyxOd24",
-    authDomain: "buy-a-book-4fb17.firebaseapp.com",
-    databaseURL: "https://buy-a-book-4fb17.firebaseio.com",
-    projectId: "buy-a-book-4fb17",
-    storageBucket: "buy-a-book-4fb17.appspot.com",
-    messagingSenderId: "583903130141",
-    appId: "1:583903130141:web:1c578a70a091c407650cf4",
-    measurementId: "G-867SZV6T81"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  const auth = firebase.auth();
+  apiKey: "AIzaSyD9K82L8GFGOkRcqQEJGNSj2PXQAyxOd24",
+  authDomain: "buy-a-book-4fb17.firebaseapp.com",
+  databaseURL: "https://buy-a-book-4fb17.firebaseio.com",
+  projectId: "buy-a-book-4fb17",
+  storageBucket: "buy-a-book-4fb17.appspot.com",
+  messagingSenderId: "583903130141",
+  appId: "1:583903130141:web:1c578a70a091c407650cf4",
+  measurementId: "G-867SZV6T81"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const auth = firebase.auth();
 
+$("#nav").on("click", function () {
+  $("nav").toggle(1000)
+})
